3f402e169ccd455aed67d8937a665bc661f79e2b
fathanpxg <fathanpxg@gmail.com>
2025-04-06 19:20:46 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Ganti dengan konfigurasi Firebase kamu
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA1bOo_bOFHXTlucScHI-bir114Ubn0yTU",
+  authDomain: "namaproject-id.firebaseapp.com",
+  projectId: "Pnamaproject-id",
+  storageBucket: "namaproject-id.firebasestorage.app",
+  messagingSenderId: "784124563275",
+  appId: "784124563275:web:c7ce4c726d3c6236e6a2ce"
 };
 
 firebase.initializeApp(firebaseConfig);
