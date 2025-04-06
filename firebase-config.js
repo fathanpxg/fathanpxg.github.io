de00c06bcc902ced0e5b30e41cc353283ec2013c
fathanpxg <fathanpxg@gmail.com>
2025-04-06 18:45:49 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Ganti dengan konfigurasi Firebase milik kamu
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
